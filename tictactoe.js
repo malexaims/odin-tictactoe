f47feb68f30b7d0f92998ba5f617731b6cffc7a2
malexaims <malexaims@gmail.com>
2024-10-22 00:19:56 +0000

    Initial js file creation

--- tictactoe.js
+++ tictactoe.js
@@ -2,5 +2,24 @@ class Player {
     constructor(name){
         this.name = name;
     }
-    const getName = () => this.name;
+    getName = () => this.name;
 }
+
+class GameBoard {
+    constructor(rows, columns){
+        this.rows = rows;
+        this.columns = columns;
+
+        this.board = [];
+        for (let i = 0; i < this.rows; i++){
+            this.board[i] = [];
+            for (let j = 0; j < this.columns, j++){
+                board[i].push(Cell());
+            }
+        }
+
+    }
+}
+
+// TODO: Create Cell class or factory function?
+// TODO: Create GameController class
