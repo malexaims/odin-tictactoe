581469b7f209bc54e05baeff651fd9af03ce29e0
malexaims <malexaims@gmail.com>
2024-10-23 21:57:47 +0000

    Implemented gamecontroller and console logging for game winner and game over

--- tictactoe.js
+++ tictactoe.js
@@ -41,7 +41,7 @@ class GameBoard {
 
 class Cell {
     constructor(){
-        this.token = 0;
+        this.token = '-';
     }
     placeTokenOnCell = (player) => {
         this.token = player.token
@@ -51,13 +51,97 @@ class Cell {
     showToken = () => this.token.toString();
 }
 
-// TODO: Create Cell class or factory function?
-// TODO: Create GameController class
+class GameController{
+    constructor(player1, player2, gameboard){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.gameboard = gameboard;
+        this.currentPlayer = player1;
+        this.turns = 0;
+        this.maxTurns = this.gameboard.rows * this.gameboard.columns;
+        this.gameOver = false;
+    }
 
-let testBoard = new GameBoard(6, 6);
+    switchPlayer = () => {
+        this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    }
+
+    checkWin = () => {
+        let board = this.gameboard.getBoard();
+        for (let r = 0; r < this.gameboard.rows; r++){
+            if (this.checkLine(board[r])) return true;
+        }
+
+        for (let c = 0; c < this.gameboard.columns; c++){
+            let column = board.map(row => row[c]);
+            if (this.checkLine(column)) return true; 
+        }
+
+        let diag1 = []; //Upper left to lower right
+        let diag2 = []; //Upper right to lower left
+        for (let i = 0; i < this.gameboard.rows; i++){
+            diag1.push(board[i][i]);
+            diag2.push(board[i][this.gameboard.columns - i - 1]);
+        }
+        if (this.checkLine(diag1) || this.checkLine(diag2)) return true;
+
+        return false;
+    }
+
+    checkLine = (line) => {
+        // Ensure all tokens are equal and not the default token '-'
+        return line.every(cell => cell.getToken() !== '-' && cell.getToken() === line[0].getToken());
+    }
+
+    playTurn = (row, column) => {
+        if (this.gameOver){
+            console.log("Game is over. Please restart.");
+            return;
+        }
+
+        if (this.gameboard.accessCell(row, column).getToken() === '-'){
+            this.gameboard.placeToken(row, column, this.currentPlayer);
+            this.turns++;
+
+            if (this.checkWin()) {
+                console.log(`Winner is ${this.currentPlayer.getName()}!`);
+                this.gameOver = true;
+            }
+            else if (this.turns === this.maxTurns){
+                console.log("Draw!");
+                this.gameOver = true;
+            }
+            else {
+                this.switchPlayer();
+            }
+        }
+        else {
+           console.log(`A player's token has already been placed at ${row}, ${column}. Choose another location.`); 
+        }
+
+    }
+
+    restartGame = () => {
+        this.gameboard.createStartingBoard();
+        this.turns = 0;
+        this.gameOver = false;
+        this.currentPlayer = this.player1;
+        console.log("Game restarted!");
+    }
+
+
+}
+
+
+let testBoard = new GameBoard(3, 3);
 testBoard.createStartingBoard();
-testBoard.printBoard();
 
-testPlayer = new Player("Bob", "X");
-testBoard.placeToken(1, 1, testPlayer);
+player1 = new Player("Bob", "X");
+player2 = new Player("Sally", "0");
+
+game = new GameController(player1, player2, testBoard);
+
+game.playTurn(0,0);
+game.playTurn(1,1)
+
 testBoard.printBoard();
